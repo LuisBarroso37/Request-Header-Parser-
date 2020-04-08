02322a6fbe988848c1354ff08fee2898c2929272
Glitch (request-header-parser-lb) <none>
2020-04-08 18:50:17 +0000

    ☔️😚 Updated with Glitch

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
-// server.js
-// where your node app starts
-
 // init project
-var express = require('express');
-var app = express();
-var accepts = require('accepts');
+const express = require('express');
+const app = express();
+const accepts = require('accepts');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// Enables style.css file
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+// Loads index.html file when you enter app
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// Request Header Parser Microservice 
-app.get("/api/:name", function (req, res) {
+// User story 1 - I can get the IP address, preferred languages (from header Accept-Language) and system infos (from header User-Agent) for my device.
+// Example Output: {"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5","software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+app.get("/api/:name", (req, res) => {
   res.json({"ipaddress": req.ip, "language": accepts(req).languages(), "software": req.get('user-agent')});
 });
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// listen for requests
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
